c6d13818e6c28e0aa6ffe1ba4c678d0757717dd5
Heinz <araqueheinz@gmail.com>
2018-04-11 15:24:03 -0400

    Browser sync done

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,24 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
  
 gulp.task('scss', function () {
   return gulp.src('scss/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('css'));
+    .pipe(gulp.dest('css'))
+    .pipe(browserSync.reload({
+        stream:true
+    }))
 });
+
+gulp.task('browser-sync', function(){
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    })
+})
  
-gulp.task('watch', function () {
-  gulp.watch('scss/*.scss', ['scss']);
+gulp.task('watch',['browser-sync', 'scss'] , function () {
+  gulp.watch('scss/**/*.scss', ['scss']);
 });
